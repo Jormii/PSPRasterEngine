c051f7a84cd82c14b39d354b21a225e197e776f6
Jorge <lopeznatalj@gmail.com>
2021-11-26 11:51:18 +0100

    Forgot to align buffer variable

--- include/engine/draw_buffer.hpp
+++ include/engine/draw_buffer.hpp
@@ -1,6 +1,8 @@
 #ifndef DRAWING_BUFFER_HPP
 #define DRAWING_BUFFER_HPP
 
+#include "vfpu.hpp"
+
 #include "types.hpp"
 #include "mesh.hpp"
 #include "vec3f.hpp"
@@ -11,7 +13,7 @@ struct BufferVertexData
     Vec3f position;
     Vec3f viewPos;
     Vec3f normal;
-    Vec4f color;
+    Vec4f VFPU_ALIGN color;
     Vec2f uv;
 };
 
